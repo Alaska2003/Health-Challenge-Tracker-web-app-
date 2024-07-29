98f822a779b808ae6a3a4243ee4314115f5967e8
Alaska <109745674+Alaska2003@users.noreply.github.com>
2024-07-29 23:49:57 +0530

    Add files via upload

---
+++ polyfills.ts
@@ -0,0 +1,29 @@
+// This file includes polyfills needed by Angular and is loaded before the app.
+// You can add your own extra polyfills to this file.
+
+import 'zone.js';  // Included with Angular CLI.
+
+// For browsers that don't support ES6
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/weak-map';
+import 'core-js/es/set';
+
+// For browsers that don't support classList
+import 'classlist.js';  // Run `npm install --save classlist.js`.
+
+// For browsers that don't support web-animations
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+
+// For browsers that don't support IntersectionObserver
+import 'intersection-observer';  // Run `npm install --save intersection-observer`.
